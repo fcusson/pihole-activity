a4d2e6dbc8141e2674b5377b94725509dc2be83f
Darkfull-Dante <60357894+Darkfull-Dante@users.noreply.github.com>
2020-01-28 11:32:16 -0500

    added install file

---
+++ install.sh
@@ -0,0 +1,4 @@
+#####################################################################
+## This script is used for the installation of the pihole-activity ##
+## project on your machine                                         ##
+#####################################################################
