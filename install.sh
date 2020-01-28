525889604cd67bd8cc48a9583e004510698c8763
Darkfull-Dante <60357894+Darkfull-Dante@users.noreply.github.com>
2020-01-28 12:27:33 -0500

    wrote the install script

--- install.sh
+++ install.sh
@@ -1,4 +1,37 @@
+#! /bin/sh
+
 #####################################################################
 ## This script is used for the installation of the pihole-activity ##
 ## project on your machine                                         ##
 #####################################################################
+
+# Set list of packages required
+PACKAGES = "wiringPi python3-dev libffi-dev libssl-dev"
+
+# update/grade aptget
+apt-get update
+apt-get upgrade -y
+apt-get $PACKAGES -y
+
+# Set location of profile file and file to launch at boot
+PROFILE = "/home/pi/profile"
+BOOT1 = "sudo python3 /home/pi/pihole-activity/src/percentCheck.py &"
+BOOT2 = "sudo python3 /home/pi/pihole-activity/src/adBlocked.py &"
+
+# Check if percentCheck.py is set at launch
+if grep -Fq $BOOT1 $PROFILE
+then
+
+# If not, write the command to the profile file
+else
+    echo $BOOT1 >> $PROFILE
+fi
+
+# Check if adBlocked.py is set at launch
+if grep -Fq $BOOT2 $PROFILE
+then
+
+# If not write the command to the profile file
+else
+    eco $BOOT2 >> $PROFILE
+fi
